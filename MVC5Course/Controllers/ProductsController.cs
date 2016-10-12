b37ed165c9856f60a0d34d3700ea0e8ea015d3fa
Will 保哥 <doggy.huang@gmail.com>
2016-10-12 21:48:12 +0800

    05 在 Products 的 CRUD 介面中都加上表單欄位驗證 (所有欄位必填)

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -17,7 +17,7 @@ namespace MVC5Course.Controllers
         // GET: Products
         public ActionResult Index()
         {
-            return View(db.Product.ToList());
+            return View(db.Product.OrderByDescending(p => p.ProductId).Take(10).ToList());
         }
 
         // GET: Products/Details/5
