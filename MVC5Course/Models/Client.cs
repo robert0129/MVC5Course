b00bbb6279adad6f18cb4f49a029ace3b3cec2d7
Will <doggy.huang@gmail.com>
2016-10-15 15:02:24 +0800

    修改 DateOfBirth 為 input type="date" 格式

--- MVC5Course/Models/Client.cs
+++ MVC5Course/Models/Client.cs
@@ -40,6 +40,7 @@ namespace MVC5Course.Models
         [DisplayName("性別")]
         public string Gender { get; set; }
         [DisplayName("生日")]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
         [DisplayName("信用評等")]
