38ef66bdd42e45eaa8d7a4f1617f8c250cddbaae
Will <doggy.huang@gmail.com>
2016-10-15 14:33:48 +0800

    12 修改 Client 模型類別的 DateOfBirth 在顯示的時候僅顯示日期部分 (不含時間)

--- MVC5Course/Models/Client.cs
+++ MVC5Course/Models/Client.cs
@@ -40,6 +40,7 @@ namespace MVC5Course.Models
         [DisplayName("性別")]
         public string Gender { get; set; }
         [DisplayName("生日")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
         [DisplayName("信用評等")]
         public Nullable<double> CreditRating { get; set; }
