30eb21778d4237306fee78b53c849bd1b966be08
chaolong.chen <chaolong.chen@gmail.com>
2016-10-29 16:15:08 +0800

    Modify Edit of Client to verify and update model with model binding and more safe

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -98,11 +98,13 @@ namespace MVC5Course.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
+        //public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
+        public ActionResult Edit(int id, FormCollection form)
         {
-            if (ModelState.IsValid)
+            var client = db.Client.Find(id);
+            if (TryUpdateModel(client, null, null, new string[] { "isAdmin"}))
             {
-                db.Entry(client).State = EntityState.Modified;
+                //db.Entry(client).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
